19a9de1c83d29e2919e3b916fdc8463284a9437e
Subashchandrabose M <52975478+Subas33@users.noreply.github.com>
2021-09-26 22:47:44 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
-const card = document.querySelector("landing-page-container");
+
+const redRadio = document.getElementById("red-radio");
+const greenRadio = document.getElementById("green-radio");
+const yellowRadio = document.getElementById("yellow-radio");
+const pinkRadio = document.getElementById("pink-radio");
+
+
+
+const clicked = (e)=>{
+    e.target.classList.toggle("radio-selected");
+    console.log("clicked");
+}
+
+redRadio.addEventListener("click", e=>{
+    e.preventDefault();
+    clicked(e);
+    
+});
+
+greenRadio.addEventListener("click", e=>{
+    e.preventDefault();
+    clicked(e);
+    
+});
+
+yellowRadio.addEventListener("click", e=>{
+    e.preventDefault();
+    clicked(e);
+    
+});
+
+pinkRadio.addEventListener("click", e=>{
+    e.preventDefault();
+    clicked(e);
+});
